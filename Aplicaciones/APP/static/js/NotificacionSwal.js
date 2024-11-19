d320edb59fa8d96c105f1cf0303017a91d4a8bd6
FRANSHESCA <U20307714@utp.edu.pe>
2024-11-18 19:44:24 -0500

    PUSH

---
+++ Aplicaciones/APP/static/js/NotificacionSwal.js
@@ -0,0 +1,74 @@
+// Obtener los parámetros de la URL
+const urlParams = new URLSearchParams(window.location.search);
+const registroExitoso = urlParams.get('registro_exitoso');
+const loginExitoso = urlParams.get('login_exitoso');
+const usuarioExistente = urlParams.get('usuario_existente');
+// Seleccionar todos los enlaces con la clase "btn-comprar"
+const enlacesComprar = document.querySelectorAll('.btn-comprar');
+
+// Mostrar alerta si el registro fue exitoso
+if (registroExitoso === 'true') {
+    Swal.fire({
+        icon: 'success',
+        title: 'Registro exitoso',
+        text: 'Por favor, inicia sesión.',
+        confirmButtonText: 'Aceptar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            // Recarga la página sin el parámetro en la URL
+            window.location.href = 'login/';
+        }
+    });
+} else if (registroExitoso === 'false') {
+    Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Hubo un problema en el registro. Inténtalo nuevamente.',
+        confirmButtonText: 'Aceptar'
+    });
+}
+
+// Mostrar alerta si el usuario ya existe
+if (usuarioExistente === 'true') {
+    Swal.fire({
+        icon: 'error',
+        title: 'Usuario existente',
+        text: 'El usuario con este correo ya existe. Intenta con otro correo.',
+        confirmButtonText: 'Aceptar'
+    });
+}
+
+// Mostrar alerta si el inicio de sesión falló
+if (loginExitoso === 'false') {
+    Swal.fire({
+        icon: 'error',
+        title: 'Error de autenticación',
+        text: 'Credenciales incorrectas. Inténtalo nuevamente.',
+        confirmButtonText: 'Aceptar'
+    });
+}
+
+// Iterar sobre cada enlace y agregar un evento de clic
+enlacesComprar.forEach((enlace) => {
+    enlace.addEventListener('click', (event) => {
+        // Prevenir que el enlace redirija
+        event.preventDefault();
+
+        // Simula la lógica de autenticación
+        const usuarioAutenticado = false; // Cambia esto según tu lógica de autenticación
+
+        if (!usuarioAutenticado) {
+            // Mostrar alerta si no está autenticado
+            Swal.fire({
+                icon: 'warning',
+                title: 'Iniciar sesión',
+                text: 'Debe iniciar sesión para poder realizar una compra.',
+                confirmButtonText: 'Aceptar'
+            });
+        } else {
+            // Lógica para continuar con la compra
+            window.location.href = '/comprar'; // Cambia "/comprar" por la URL real de compra
+        }
+    });
+});
+
--- Aplicaciones/APP/urls.py
+++ Aplicaciones/APP/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from Aplicaciones.APP.views import * 
+from . import views
 
 urlpatterns = [
     path('', PromocionListView.as_view(), name  = 'gestion_promocion'),
     path('login/', LoginListView.as_view(), name  = 'gestion_login'),
+    path('registrarUsuario/', views.registrarUsuario),
+    path('validarUsuario/', views.validarUsuario),
+    path('pagina_inicio/', pagina_inicio),
+    path('cerrar_sesion/', views.cerrar_sesion),
+    path('configuracion_usuario/', configuracion_usuario),
+    path('datos_personales/', datos_personales),
+    path('historial_compras/', historial_compras),
+    path('editar_usuario/', views.editar_usuario, name='editar_usuario'),
 ]
